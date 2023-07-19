67f99b910e4d9da136e208822d0725b410c2414e
Fernando-Ruiz176 <ruizfe.sud@gmail.com>
2023-07-19 02:52:18 -0400

    Creacion de las funcionalidades

--- app/src/main/java/cl/fernando/individual_14/MainActivity.kt
+++ app/src/main/java/cl/fernando/individual_14/MainActivity.kt
@@ -1,11 +1,35 @@
 package cl.fernando.individual_14
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import cl.fernando.individual_14.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var saldo = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnEnviar.setOnClickListener{
+            when(binding.radioGroup.checkedRadioButtonId){
+                R.id.btnVerSaldo ->
+                    binding.editTextMonto.setText(saldo.toString())
+                R.id.btnIngresar -> ingresarSaldo()
+
+            }
+        }
+
+    }
+
+    private fun ingresarSaldo() {
+        saldo += binding.editTextMonto.text.toString().toInt()
+        Toast.makeText(applicationContext,"Su saldo ha sido actualizado correctamente", Toast.LENGTH_LONG).show()
+
     }
 }
